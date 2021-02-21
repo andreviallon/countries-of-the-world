87f7fed816b21cf31872114c6a80393be8249c98
Andre Viallon <viallon.andre@gmail.com>
2021-02-21 18:44:59 +0100

    Get server to render UI with styling

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,24 @@
+import React, { useEffect } from 'react'
+import { CssBaseline, ThemeProvider } from '@material-ui/core'
 import Layout from '../components/Layout'
 import '../styles/globals.scss'
+import theme from '../styles/theme'
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Layout>
+			<Component {...pageProps} />
+		</Layout>
+      </ThemeProvider>
   )
 }
 
